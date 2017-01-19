ee737fef092d8d9fce6173a474cc777b0faea64c
H3957 <runskiing@hotmail.com>
2017-01-19 12:11:20 +0200

    t5 kesken

---
+++ demo01/demo01T5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo01T5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Tee ohjelma, joka näyttää annetun sekuntimäärän tunteina, minuutteina ja sekunteina. 
+            Aikamääre sekuntteina kysytään käyttäjältä.*/
+
+            int koksek;
+
+            int sek;
+            int min;
+            int h;
+            Console.WriteLine("Anna aika sekunteina: ");
+            koksek = int.Parse(Console.ReadLine());
+
+            while (koksek != 0)
+                sek = (koksek % 60);
+                min = (koksek % 3600) / 60;
+                h = koksek / 3600;
+            Console.WriteLine("Anna sekunttiaika voidaan ilmaista muodossa: " +koksek);
+
+        }
+    }
+}
