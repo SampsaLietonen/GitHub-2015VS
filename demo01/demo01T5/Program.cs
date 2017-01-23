5a13d9589361870f95d0c85656bfa878583d627f
sampsa <runskiing@hotmail.com>
2017-01-23 15:15:34 +0200

    tällä hetkellä staattinen ohjelma

--- demo01/demo01T5/Program.cs
+++ demo01/demo01T5/Program.cs
@@ -12,14 +12,18 @@ namespace demo01T5
         {
             /*Tee ohjelma, joka näyttää annetun sekuntimäärän tunteina, minuutteina ja sekunteina. 
             Aikamääre sekuntteina kysytään käyttäjältä.*/
-            static string Method1(int secs)
-            {
-                int hours = secs / 3600;
-                int mins = (secs % 3600) / 60;
-                secs = secs % 60;
-                return string.Format("{0:D2}:{1:D2}:{2:D2}", hours, mins, secs);
-            }
+            //double sekunnit;
 
+            Console.Write("Give seconds > ");
+            double sekunnit = double.Parse(Console.ReadLine());
+
+            //timepsan FromsSeconds metodilla muuntaminen
+            //tällä hetkellä staattinen luku
+            var timeSpan = TimeSpan.FromSeconds(7200);
+            int hh = timeSpan.Hours;
+            int mm = timeSpan.Minutes;
+            int ss = timeSpan.Seconds;
+            Console.WriteLine("Hours: " + hh + " Minutes: " + mm + " Seconds: " + ss);
         }
     }
 }
