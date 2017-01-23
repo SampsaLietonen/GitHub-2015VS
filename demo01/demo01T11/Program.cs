56287a727e10c8b7f997713e4532c561754db04b
sampsa <runskiing@hotmail.com>
2017-01-23 22:10:13 +0200

    t11 valmis

---
+++ demo01/demo01T11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo01T11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //tulostaa tähtiä käyttäjän syöttämän luvun mukaan
+
+            int luku;
+            string tahti = "*";
+            Console.Write("Anna luku: ");
+            luku = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= luku; i++) //luuppi annetun luvun läpikäyntiin
+            {
+
+                for (int s = 1; s <= i; s++) //looppi tähtien tulostukselle
+                {
+                    Console.Write(tahti);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
