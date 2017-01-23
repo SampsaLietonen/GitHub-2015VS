4b7af192ffed163d3064b6d0c88929fb4edd03e9
sampsa <runskiing@hotmail.com>
2017-01-23 19:43:59 +0200

    T7 valmis

---
+++ demo01/demo01T7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo01T7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tee ohjelma, joka näyttää onko annettu vuosi karkausvuosi. Vuosiluku kysytään käyttäjältä.
+            int vuosi;
+            Console.Write("Anna vuosi >");
+            vuosi = int.Parse(Console.ReadLine());
+
+            // jos vuosi on 400 jaollinen ->  karkausvuosi
+            if (vuosi % 400 == 0)
+            {
+                Console.Write("Vuosi on karkausvuosi" + "\n");
+            }
+            else
+            {
+                Console.Write("Vuosi ei ole karkausvuosi" +"\n");
+            }
+        }
+    }
+}
