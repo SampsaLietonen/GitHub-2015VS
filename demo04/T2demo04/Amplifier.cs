b0c57c6a6759d04d64a790a141c3be07b087a84e
H3957 <runskiing@hotmail.com>
2017-02-02 11:40:12 +0200

    T2 demo04 valmis

---
+++ demo04/T2demo04/Amplifier.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2demo04
+{
+    class Amplifier
+    {
+        //properties
+        private static int MaxVolume = 100;
+        private static int MinVolume = 0;
+        private int volume;
+        public int Volume
+        {
+            get {return volume;}
+            set {
+                if (value >= MinVolume && value <= MaxVolume) volume = value;
+
+                else if (value < MinVolume)
+                {
+                    Console.WriteLine("Too low volume - Amplifier is set to minimun: " +MinVolume);
+                    volume = MinVolume;
+                }
+
+                else if (value > MaxVolume)
+                {
+                    Console.WriteLine("Too high volume - Amplifier is set to maximum: " + MaxVolume);
+                    volume = MaxVolume;
+                }
+            }
+        } 
+    }
+}
---
+++ demo04/T2demo04/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2demo04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Amplifier pioneer = new Amplifier();
+            int input;
+
+            do
+            {
+                Console.WriteLine("Give a volume between 0-100: ");
+                input = int.Parse(Console.ReadLine());
+                pioneer.Volume = input;
+                Console.WriteLine("Amplifier volume is set to: " + pioneer.Volume);
+
+            }
+            while (input != -1 );
+        }
+    }
+}
