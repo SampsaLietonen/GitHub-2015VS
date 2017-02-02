147fb12d36cac1fda403b1f98504ae8e33e359dd
H3957 <runskiing@hotmail.com>
2017-02-02 11:06:53 +0200

    demo04 T1 valmis

---
+++ demo04/T1demo04/Elevator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1demo04
+{
+    class Elevator
+    {
+        //properties
+        private static int MaxFloor = 5;
+        private static int MinFloor = 1;
+        private int floor;
+        public int Floor
+        {
+            get { return floor; }
+            set {
+                if (value >= MinFloor && value <= MaxFloor) floor = value;
+
+                else if (value < MinFloor)
+                    Console.WriteLine("Floor number is too small!");
+
+                else if (value > MaxFloor)
+                    Console.WriteLine("Floor number is too big!");
+
+            }
+
+           }
+    }
+}
---
+++ demo04/T1demo04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1demo04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elevator elevator = new Elevator();
+            int input;
+            //Console.WriteLine("Floor number is: " + elevator.getFloor());
+
+
+            //Console.WriteLine("Elevator is on floor : " + elevator.getFloor());
+
+            do
+            {
+                Console.WriteLine("Give a new floor number 1 to 5: ");
+                input = int.Parse(Console.ReadLine());
+                elevator.Floor = input;
+                Console.WriteLine("Floor number is: " + input);
+
+
+            }
+            while (input != 0);
+
+        }
+
+    }
+}
