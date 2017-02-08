e551f9959833fa9d9d9d07f705a17b048820ee2a
sampsa <sampsa@SAME-LAPTOP>
2017-02-08 21:11:57 +0200

    T5 valmis

---
+++ demo04/T5demo04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T5demo04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //creat one radio Object using parametric constructor
+            Console.WriteLine("Radio one");
+            Radio radio = new Radio(true, 8, 2000.0);
+            Console.WriteLine(radio.ToString());
+            Console.WriteLine("\n");
+
+            //creat another radio Object using parametric constructor
+            Console.WriteLine("Radio two");
+            Radio radio2 = new Radio(true, 10, 2700.0);
+            Console.WriteLine(radio2.ToString());
+            Console.WriteLine("\n");
+
+            //creat another radio Object using parametric constructor
+            Console.WriteLine("Radio three");
+            Radio radio3 = new Radio(true, -1, 1600.0);
+            Console.WriteLine(radio3.ToString());
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ demo04/T5demo04/Radio.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T5demo04
+{
+    class Radio
+    {
+        //properties
+        public bool On { get; set; }
+        private static int MaxVolume = 9;
+        private static int MinVolume = 0;
+        private int volume;
+        public int Volume
+        {
+            get { return volume; }
+            set {
+                if (value >= MinVolume && value <= MaxVolume) volume = value;
+
+                else if (value < MinVolume) volume = MinVolume;
+                    //Console.WriteLine("Turn volumebutton to the other dircetion!");
+
+                else if (value > MaxVolume) volume = MaxVolume;
+                    //Console.WriteLine("Volume 9 is the maximum!");
+
+            }
+                
+        }
+
+        private static double minF = 2000.0;
+        private static double maxF = 2600.0;
+        private double frequency;
+        public double Frequency {
+            get { return frequency; }
+            set {
+                if (value >= minF && value <= maxF) frequency = value;
+
+                else if (value < minF) frequency = minF;
+                    //Console.WriteLine("Too low frequency!");
+
+                else if (value > maxF) frequency = maxF;
+                    //Console.WriteLine("Too high frequency");
+            }
+        }
+
+        //default constructor
+        public Radio()
+        {
+        }
+
+        //parametric constructor
+        public Radio(bool on, int volume, double frequency)
+        {
+            On = on;
+            Volume = volume;
+            Frequency = frequency;
+        }
+
+        //return Radio data as a one string, override Object class ToString()- method
+        public override string ToString()
+        {
+            return "Radio is on: " + On + " " + "Volume: " + Volume + " " + "Frequency: " + Frequency + " ";
+        }
+    }
+}
