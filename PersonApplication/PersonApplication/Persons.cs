38aad3bc7e0451ec4d02c3a4307b589b94257a48
H3957 <h3957@student.jamk.fi>
2017-02-09 11:00:33 +0200

    t1 valmis

---
+++ PersonApplication/PersonApplication/Persons.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApplication
+{
+    class Persons
+    {
+        //persons collection stores Person Object
+        private List<Person> persons;
+
+        //constructor
+        public Persons()
+        {
+            persons = new List<Person>();
+        }
+        
+        /// <summary>
+        /// Add persons to collection
+        /// </summary>
+        /// <param name="person">person to add</param>
+        public void AddPerson(Person person)
+        {
+            persons.Add(person);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public Person GetPerson(int index)
+        {
+            if (index < persons.Count) return persons.ElementAt(index);
+            else return null;
+        }
+
+        public Person FindPerson(string SocialSecurityNumber)
+        {
+            foreach (Person person in persons)
+            {
+                //compare to string
+                if (SocialSecurityNumber.Equals(person.SocialSecurityNumber))
+                {
+                    return person;
+                }
+            }
+            return null; //person with search socialsecuritynumber not found in collection
+        }
+
+        public void PrintData()
+        {
+            foreach(Person person in persons)
+            {
+                Console.WriteLine(person.ToString());
+            }        
+        }
+
+    }
+}
---
+++ PersonApplication/PersonApplication/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create persons (collection)
+            Persons myFriends = new Persons();
+
+            //create few persons Object
+            Person person1 = new Person { Firstname = "Kirsi", Lastname = "Kernell", SocialSecurityNumber = "123456-123A"};
+
+            Person person2 = new Person { Firstname = "Matti", Lastname = "Kattinen", SocialSecurityNumber = "123457-127B" };
+            Person person3 = new Person { Firstname = "Veikko", Lastname = "Peikko", SocialSecurityNumber = "123458-127C" };
+
+            //add Person Object to Persons (myFriends)
+            myFriends.AddPerson(person1);
+            myFriends.AddPerson(person2);
+            myFriends.AddPerson(person3);
+
+            //get one person
+            Person person4 = myFriends.GetPerson(0);
+            if (person4 != null)
+            {
+                Console.WriteLine(person4.ToString());
+            }
+            else
+            {
+                Console.WriteLine("There is no person at that position!");
+            }
+
+            //print collection
+            myFriends.PrintData();
+
+            //find person
+            string SocialSecurityNumber = "123457-127";
+            Console.WriteLine("Find person with SocialSecurityNumber:" + SocialSecurityNumber);
+            Person person5 = myFriends.FindPerson(SocialSecurityNumber);
+            if (person5 != null)
+            {
+                Console.WriteLine(person5.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Can´t find person with that SocialSecurityNumber!");
+            }
+        }
+    }
+}
